2956dfdfba74e0f3fc6f641554b887e0222f6cd9
oleksii.svarychevskyi <oleksii.svarychevskyi@de.atu.eu>
2017-09-12 10:47:34 +0200

    - fixed unit tests

--- test/cli/http-cli.test.js
+++ test/cli/http-cli.test.js
@@ -1,4 +1,27 @@
-jest.mock('node-fetch');
+import axios from 'axios';
+jest.mock('axios');
+
+const fileSystem = {
+    'https://localhost/reference/1_reference.yaml': `
+        key: value1
+        reference: 
+            $ref: cms://reference/2_reference.yaml
+    `,
+    'https://localhost/reference/2_reference.yaml': `key: value2`
+};
+
+axios.mockImplementation(
+    (path) => {
+        if (fileSystem[path]) {
+            return Promise.resolve({ text:() => fileSystem[path] });
+        } else {
+            return Promise.reject({ error: `Directory with ${path} was not found.` });
+        }
+    });
+
+beforeEach(() => {
+    axios.mockClear();
+});
 
 import HttpCli from '../../src/cli/http-cli';
 

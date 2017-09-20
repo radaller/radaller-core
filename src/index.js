94236021560928dd7f2fa2ef3fa219c0a2f6f96c
oleksii.svarychevskyi <oleksii.svarychevskyi@de.atu.eu>
2017-09-20 14:52:20 +0200

    - moved index.js to src folder

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export GitHubCms from './cms/GitHubCms';
+export FileCms from './cms/FileCms';
+export FileCli from './cli/FileCli';
+export HttpCli from './cli/HttpCli';
+export { GitHubUnauthorisedError, GitHubTokenExistError, GitHubTwoFactorError } from './cms/github/GitHubError';
